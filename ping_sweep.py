061bf530297cc324f5d0fa42e1cb33fd2da715bd
urslimshady <82615170+urslimshady@users.noreply.github.com>
2023-03-26 16:15:44 +0530

    Uploaded a code snippet
    This is a sample code for pinging a system to get info about opened ports.

---
+++ ping_sweep.py
@@ -0,0 +1,38 @@
+import os
+import platform
+
+from datetime import datetime
+net = input("Enter the network address: ")
+net1 = net.split('.')
+a = '.'
+
+net2 = net1[0] + a + net1[1] + a + net1[2] + a
+st1 = int(input("Enter the starting number: "))
+en1 = int(input("Enter the last number: "))
+en1 = en1 + 1
+operation = platform.system()
+
+if(operation == "Windows"):
+    ping1 = "ping -n 1 "
+elif(operation == "Linux" ):
+    ping1 = "ping -c 1 "
+else:
+    ping1 = "ping -c 1 "
+t1 = datetime.now()
+print("Scanning in progress...")
+
+for ip in range(st1, en1):
+    address = net2 + str(ip)
+    command = ping1 + address
+    response = os.popen(command)
+
+    for line in response.readlines():
+        if(line.count("TTL")):
+            break
+        if(line.count("TTL")):
+            print(address, "-->live")
+
+t2 = datetime.now()
+total = t2 -t1
+print("Scanning completed in: ", total)
+
